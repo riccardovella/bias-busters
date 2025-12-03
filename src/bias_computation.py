dec8404f5a9fe6bdc352d46cba0955d931fea15b
riccardovella <riccardo.vella@imtlucca.it>
2025-12-03 11:51:17 +0100

    Added placeholder function for computing bias

--- src/bias_computation.py
+++ src/bias_computation.py
@@ -5,3 +5,8 @@
 # Here there should be the logic for computing bias scores given 
 # a set of target and attribute words using contextual embeddings.
 #######################################################
+
+def compute_bias(model, sentence, subject_words, attribute_words):
+    # sentence has to include "[SUBJ]", "[ATTR]"
+    # Placeholder for bias computation logic
+    pass
